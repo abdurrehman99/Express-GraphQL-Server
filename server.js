eacb2b9d183dbe44502092cd95af8a1798903003
abdurrehman99 <syed.ebad1999@gmail.com>
2021-02-09 01:14:33 +0500

    mongodb connection

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+
+//Load env vars
+dotenv.config({ path: "./.env" });
+
+//Default port for NODE app
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 app.use(
-	"/",
-	graphqlHTTP({
-		schema: buildSchema(`
+  "/",
+  graphqlHTTP({
+    schema: buildSchema(`
         type RootQuery {
             events : [String!]!
         }
@@ -24,17 +31,28 @@ app.use(
             mutation: RootMutation
         }
     `),
-		rootValue: {
-			events: () => {
-				return ["Cooking", "Reading"];
-			},
-			createEvent: (args) => {
-				const eventName = args.name;
-				return eventName;
-			},
-		},
-		graphiql: true,
-	})
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Reading"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
 );
 
-app.listen(5000, () => console.log("GraphQL server running"));
+//Connect to Mongo DB then Start server
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT);
+    console.log("MongoDB is connected\nGraphQL server running");
+  })
+  .catch((error) => console.log(`Can't Establish connection with DB`, error));
