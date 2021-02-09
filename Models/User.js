6ac20aaffeb91f12c9f79da2a59d8972999a53a5
abdurrehman99 <syed.ebad1999@gmail.com>
2021-02-09 19:31:21 +0500

    addnewTodo query

--- Models/User.js
+++ Models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  passowrd: {
+  password: {
     type: String,
   },
   todos: {
--- server.js
+++ server.js
@@ -22,50 +22,66 @@ app.use(
   graphqlHTTP({
     schema: buildSchema(`
         type RootQuery {
-            events : [String!]!
+          events : [String!]!
         }
 
         type RootMutation {
-            createUser(email: String,password: String): String ,
-            loginUser(email: String,password: String): String 
+          createUser(email: String,password: String): String,
+          loginUser(email: String,password: String): String,
+          addNewTodo(email: String,name: String,description: String): String,
         }
 
         schema {
-            query: RootQuery
-            mutation: RootMutation
+          query: RootQuery
+          mutation: RootMutation
         }
     `),
     rootValue: {
-      events: () => {
-        return ["Cooking", "Reading"];
+      addNewTodo: async ({ email, name, description }) => {
+        try {
+          let newTodo = { name, description };
+          await User.findOneAndUpdate({ email }, { $push: { todos: newTodo } });
+          return "New todo added";
+        } catch (e) {
+          throw new Error(e.message);
+        }
       },
       createUser: async ({ email, password }) => {
-        let hashedPassword = await bcrypt.hashSync(password, 10);
-        const newUser = new User({
-          email,
-          password: hashedPassword,
-        });
-        await newUser.save();
-        return "User created";
+        try {
+          let hashedPassword = await bcrypt.hashSync(password, 10);
+          const newUser = new User({
+            email,
+            password: hashedPassword,
+          });
+          await newUser.save();
+          return "User created";
+        } catch (e) {
+          throw new Error(e.message);
+        }
       },
       loginUser: async ({ email, password }) => {
-        let user = await User.findOne({ email });
-        if (user) {
-          console.log("user", user);
-          let passwordMatched = await bcrypt.compareSync(
-            password,
-            user.password
-          );
-          if (passwordMatched) {
-            console.log("passwordMatched", passwordMatched);
-            const token = await jwt.sign({ email }, process.env.JWT_SECRET, {
-              expiresIn: "2h",
-            });
-            console.log("token", token);
-            return token;
+        try {
+          let user = await User.findOne({ email });
+          if (user) {
+            let passwordMatched = await bcrypt.compareSync(
+              password,
+              user.password
+            );
+            if (passwordMatched) {
+              console.log("passwordMatched", passwordMatched);
+              const token = await jwt.sign({ email }, process.env.JWT_SECRET, {
+                expiresIn: "2h",
+              });
+              console.log("token", token);
+              return token;
+            } else {
+              throw new Error("Invalid email (or) password");
+            }
+          } else {
+            throw new Error("User not found");
           }
-        } else {
-          return "User not found";
+        } catch (e) {
+          throw new Error(e.message);
         }
       },
     },
@@ -79,6 +95,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => {
     app.listen(PORT);
